42d3f7ac9739ff87c1ba2954dcb35a8c7a280883
Kislay Verma <kislay.verma@myntra.com>
2016-09-11 20:22:28 +0530

    Seed commit for postgreSQL support

---
+++ rulette-postgres-provider/src/main/resources/sql/setup.sql
@@ -0,0 +1,19 @@
+
+CREATE TABLE rule_system (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `table_name` varchar(100) NOT NULL,
+  `output_column_name` varchar(256) DEFAULT NULL,
+  `unique_id_column_name` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `rule_input` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `rule_system_id` int(11) NOT NULL,
+  `priority` int(11) NOT NULL,
+  `rule_type` varchar(45) NOT NULL,
+  `data_type` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+);
