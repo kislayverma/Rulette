003e867546fc02844acb4a3357174a5c0b4ab15f
Kislay Verma <kislayv@uber.com>
2020-04-25 22:54:49 +0530

    Minor changes

--- rulette-mysql-provider/src/main/java/com/github/kislayverma/rulette/mysql/util/Utils.java
+++ rulette-mysql-provider/src/main/java/com/github/kislayverma/rulette/mysql/util/Utils.java
@@ -1,22 +1,25 @@
 package com.github.kislayverma.rulette.mysql.util;
 
 import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Properties;
 
 /**
+ * Utility file for reading config and translating them to different configurations.
  *
  * @author kislay
  */
 public class Utils {
+    private static final String PROPERTY_MYSQL_DRIVER_CLASS = "driverClass";
+    private static final String PROPERTY_JDBC_URL = "jdbcUrl";
+    private static final String PROPERTY_USER_NAME = "username";
+    private static final String PROPERTY_PASSWORD = "password";
+    private static final String PROPERTY_MAX_POOL_SIZE = "maxPoolSize";
+    private static final String PROPERTY_CONN_TIMEOUT = "connectionTimeout";
 
     /**
      * Read a properties file from the class path and return a Properties object
@@ -39,18 +42,26 @@ public class Utils {
         return props;
     }
 
+    /**
+     * This method build a {@link HikariConfig} object from the given properties file.
+     * @param fileName A property file containing the Hikari configurations
+     */
     public static HikariConfig getHikariConfig(String fileName) throws IOException {
         return getHikariConfig(Utils.readProperties(fileName));
     }
 
+    /**
+     * This method build a {@link HikariConfig} object from the given properties file.
+     * @param props An {@link Properties} object encapsulating Hikari properties
+     */
     public static HikariConfig getHikariConfig(Properties props) {
         HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setDriverClassName(props.getProperty("driverClass"));
-        hikariConfig.setJdbcUrl(props.getProperty("jdbcUrl"));
-        hikariConfig.setUsername(props.getProperty("username"));
-        hikariConfig.setPassword(props.getProperty("password"));
-        hikariConfig.setMaximumPoolSize(new Integer((String) props.getProperty("maxPoolSize")));
-        hikariConfig.setConnectionTimeout(new Long((String) props.getProperty("connectionTimeout")));
+        hikariConfig.setDriverClassName(props.getProperty(PROPERTY_MYSQL_DRIVER_CLASS));
+        hikariConfig.setJdbcUrl(props.getProperty(PROPERTY_JDBC_URL));
+        hikariConfig.setUsername(props.getProperty(PROPERTY_USER_NAME));
+        hikariConfig.setPassword(props.getProperty(PROPERTY_PASSWORD));
+        hikariConfig.setMaximumPoolSize(Integer.parseInt(props.getProperty(PROPERTY_MAX_POOL_SIZE)));
+        hikariConfig.setConnectionTimeout(Long.parseLong(props.getProperty(PROPERTY_CONN_TIMEOUT)));
 
         return hikariConfig;
     }
